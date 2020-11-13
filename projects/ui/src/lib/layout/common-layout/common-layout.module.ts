e5794150ed626cfa63e2fbe0a51694d4a539b930
gablyazov <gablyazov@web3tech.ru>
2020-11-13 23:00:39 +0300

    verstka up

--- projects/ui/src/lib/layout/common-layout/common-layout.module.ts
+++ projects/ui/src/lib/layout/common-layout/common-layout.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { CommonLayoutComponent } from './common-layout.component'
 import { RouterModule } from '@angular/router'
+import { NgxdModule } from '@ngxd/core'
 
 @NgModule({
   declarations: [CommonLayoutComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, NgxdModule],
   exports: [CommonLayoutComponent]
 })
 export class CommonLayoutModule {}
